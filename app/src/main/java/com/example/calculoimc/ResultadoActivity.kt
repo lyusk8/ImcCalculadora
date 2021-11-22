d7844f7957df64c348f5953fe4f49c1bc15d1067
lyusk8 <lyusk8@gmail.com>
2021-11-22 18:32:19 -0300

    implementadas todos as funcionalidades

--- app/src/main/java/com/example/calculoimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/calculoimc/ResultadoActivity.kt
@@ -1,11 +1,22 @@
 package com.example.calculoimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class ResultadoActivity : AppCompatActivity() {
+class ResultadoActivity: AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val texto = findViewById<TextView>(R.id.texto)
+
+        val dados = intent.extras
+        val nome = dados?.getString("nome")
+        val resultado = dados?.getString("resultado")
+        texto.text = "$nome o resultado do seu imc é: $resultado"
     }
 }
--- app/src/main/java/com/example/calculoimc/activities/MainActivity.kt
+++ app/src/main/java/com/example/calculoimc/activities/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.example.calculoimc.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.calculoimc.R
+import com.example.calculoimc.ResultadoActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configBotao()
+    }
+
+    private fun configBotao(){
+        val botao = findViewById<Button>(R.id.botao)
+        botao.setOnClickListener {
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+            val nome = findViewById<TextInputEditText>(R.id.nome)
+
+            intent.putExtra("resultado", calculaImc())
+            intent.putExtra("nome", nome.text.toString())
+
+            startActivity(intent)
+
+        }
+    }
+
+    private fun calculaImc():String{
+        val peso = findViewById<TextInputEditText>(R.id.peso).text.toString().toFloat()
+        val altura = findViewById<TextInputEditText>(R.id.altura).text.toString().toFloat()
+        val numero = peso / (altura*altura)
+        return when {
+            numero > 40 -> {
+                "Obesidade grau III."
+            }
+            numero > 35 -> {
+                "Obesidade grau II"
+            }
+            numero > 30 -> {
+                "Obesidade grau I"
+            }
+            numero > 25 -> {
+                "Sobrepeso"
+            }
+            numero > 18.6 -> {
+                "Peso normal"
+            }
+            else -> {
+                "Baixo peso"
+            }
+        }
     }
 }
